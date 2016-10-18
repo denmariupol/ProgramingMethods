ff92cc7d98315bc5f26e28b7a15389d15a8b179a
den <denmariupol@mail.ru>
2016-10-18 13:16:19 +0300

    frequence of the numbers

--- src/Chapter_1_OOP/VariantB/Task.java
+++ src/Chapter_1_OOP/VariantB/Task.java
@@ -2,8 +2,7 @@ package Chapter_1_OOP.VariantB;
 
 
 import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
 
 /**
  * Created by User on 16.10.16.
@@ -15,21 +14,23 @@ public class Task {
         ArrayList<Double> list1 = new ArrayList<>();
         ArrayList<Integer> list2 = new ArrayList<>();
         for (int i = 0; i <= 20; i++) {
-            list.add(rand.nextInt(200) - 100);
+            list.add(rand.nextInt(20));
             list1.add(rand.nextDouble() * (200 - 100) - 150);
-            if(i < 2)
+            if (i < 2)
                 list2.add(rand.nextInt(100));
         }
 
-       // NegPos(list);
+        // NegPos(list);
         //MinMax(list);
         //ThreeOrNine(list);
         //FiveAndSeven(list);
         //BubbleSort(list);
         //RepetingNumbers(list1);
-        NodNok(list2);
-        SimpleNumbers(list);
+//        NodNok(list2);
+//        SimpleNumbers(list);
+        NumberFrequency(list);
     }
+
 
     private static void NegPos(ArrayList<Integer> list) {
         System.out.print("Negative numbers : ");
@@ -121,17 +122,17 @@ public class Task {
         DecimalFormat decimalFormat = new DecimalFormat(pattern);
         for (int i = 0; i < list1.size(); i++) {
             String[] s = decimalFormat.format(list1.get(i)).split(",");
-            char []ch = s[1].toCharArray();
+            char[] ch = s[1].toCharArray();
 
             int c = 0;
             for (int j = 0; j < ch.length - 1; j++) {
-                for (int k = j+1;k<ch.length;k++){
-                    if(ch[j] == ch[k])
+                for (int k = j + 1; k < ch.length; k++) {
+                    if (ch[j] == ch[k])
                         c++;
                 }
             }
             if (c == 0)
-                System.out.print(decimalFormat.format(list1.get(i))+" ");
+                System.out.print(decimalFormat.format(list1.get(i)) + " ");
 
         }
     }
@@ -141,15 +142,15 @@ public class Task {
         int nok = list.get(0);
         int nod = nok;
 
-        for (int i = 1; i < list.size(); i++)
-        {
+        for (int i = 1; i < list.size(); i++) {
             nod = GetNod(nod, list.get(i));
 //            nok = getNOK(nok, arr[i]); // функции getNOK и getNOD для двух чисел
         }
-        System.out.println("Nod : "+nod);
+        System.out.println("Nod : " + nod);
     }
-    private static int GetNod(int a,int b) {
-        for(;;) {
+
+    private static int GetNod(int a, int b) {
+        for (; ; ) {
             if (a > b)
                 a = a % b;
             else
@@ -159,21 +160,103 @@ public class Task {
                 return a + b;
         }
     }
-    private static int GetnNok(int a,int b){
+
+    private static int GetnNok(int a, int b) {
         return 0;
     }
 
 
     public static void SimpleNumbers(ArrayList<Integer> list) {
         System.out.println("Simple numbers in array : ");
-        for(Integer number : list){
+        for (Integer number : list) {
             for (int j = 2; j <= number; j++) {
                 if ((number % j == 0) && (j < number))
                     break;
                 else if ((number % j == 0) && (j == number))
-                    System.out.print(number+" ");
+                    System.out.print(number + " ");
             }
 
         }
     }
+
+
+    private static void NumberFrequency(ArrayList<Integer> list) {
+        ArrayList<Number> numbers = new ArrayList<Number>();
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i)+" ");
+
+        }
+        System.out.println();
+        for(int i = 0;i < list.size();i++){
+            int count = 0;
+            for (int j = 0;j < list.size();j++){
+                if (list.get(i) == list.get(j))
+                    count++;
+
+            }
+            numbers.add(new Number(count,list.get(i)));
+        }
+        ClearList(numbers);
+    }
+
+    private static void ClearList(ArrayList<Number> numbers) {
+        for (int i = 0; i <numbers.size(); i++) {
+            for (int j = i+1; j < numbers.size(); j++) {
+                if(numbers.get(i).val == numbers.get(j).val)
+                    numbers.remove(numbers.get(j));
+            }
+        }
+        Sort(numbers);
+    }
+
+    private static void Sort(ArrayList<Number> num) {
+            int count = 0;
+            for (int i = 0; i < num.size() - 1; i++) {
+                if (Comp(num.get(i),num.get(i + 1))) {
+                    Number temp = num.get(i);
+                    num.set(i, num.get(i + 1));
+                    num.set(i + 1, temp);
+                    count++;
+                }
+            }
+            if (count != 0)
+                Sort(num);
+            else {
+                System.out.println();
+                System.out.println("Bubble sort...");
+                for (int j = 0; j < num.size(); j++) {
+                    System.out.print(num.get(j).count + "->"+num.get(j).val+" ");
+                }
+            }
+
+
+
+//        for (int i = 0; i < num.size(); i++) {
+//            System.out.print(num.get(i).count + "->" + num.get(i).val + " ");
+//        }
+    }
+    private static boolean Comp(Number n1, Number n2) {
+        if(n2.count > n1.count)
+            return true;
+        else
+            return false;
+    }
+    private static class Number{
+        private int count;
+        private int val;
+
+        public int getCount() {
+            return count;
+        }
+
+        public int getVal() {
+            return val;
+        }
+        Number(int count,int val){
+            this.count = count;
+            this.val = val;
+        }
+    }
+
+
 }
