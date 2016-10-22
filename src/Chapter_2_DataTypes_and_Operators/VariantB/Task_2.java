154d060ef1723651710d2e171c063e96048d5b86
unknown <User@.(none)>
2016-10-22 23:07:17 +0400

    Chapter_2_VariantB_Task_2_compete

--- src/Chapter_2_DataTypes_and_Operators/VariantB/Task_2.java
+++ src/Chapter_2_DataTypes_and_Operators/VariantB/Task_2.java
@@ -1,7 +1,47 @@
 package Chapter_2_DataTypes_and_Operators.VariantB;
 
+import java.io.BufferedReader;
+
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 /**
  * Created by User on 22.10.16.
  */
 public class Task_2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i=0,j=0;
+
+        System.out.print("Введите кол-во столбцов : ");
+        if (scan.hasNextInt())
+            i = scan.nextInt();
+        else
+            System.out.print("Неверный формат ввода");
+
+        System.out.print("Введите кол-во строк : ");
+        if (scan.hasNextInt())
+            j = scan.nextInt();
+        else
+            System.out.print("Неверный формат ввода");
+
+        Matrix(i,j);
+    }
+    private static void Matrix(int i,int j){
+        int [][] matrix = new int[i][j];
+        int number =0;
+        for (int k = 0; k < i; k++) {
+            for (int l = 0; l < j; l++) {
+                matrix[k][l] = number;
+                number++;
+            }
+        }
+        for (int k = 0; k < i; k++) {
+            for (int l = 0; l < j; l++) {
+                System.out.printf("%4d",matrix[k][l]);
+                if (l == j-1)
+                    System.out.println();
+            }
+        }
+    }
 }
--- src/Chapter_2_DataTypes_and_Operators/ss.java
+++
@@ -1,7 +0,0 @@
-package Chapter_2_DataTypes_and_Operators;
-
-/**
- * Created by User on 22.10.16.
- */
-public class ss {
-}
