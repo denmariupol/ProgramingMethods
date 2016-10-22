c6d3de322ab757d340a055fd0fcef9cef9b65813
unknown <User@.(none)>
2016-10-22 22:25:35 +0400

    Chapter_2_VariantB_Task_2

--- src/Chapter_2_DataTypes_and_Operators/VariantA/Task_7.java
+++
@@ -1,7 +0,0 @@
-package Chapter_2_DataTypes_and_Operators.VariantA;
-
-/**
- * Created by User on 22.10.16.
- */
-public class Task_7 {
-}
---
+++ src/Chapter_2_DataTypes_and_Operators/ss.java
@@ -0,0 +1,7 @@
+package Chapter_2_DataTypes_and_Operators;
+
+/**
+ * Created by User on 22.10.16.
+ */
+public class ss {
+}
