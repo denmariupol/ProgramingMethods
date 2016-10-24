63a5307c7c681dc0c1b142a46d55b2cd43dde388
den <denmariupol@mail.ru>
2016-10-24 12:55:29 +0300

    chapter2_C_tTask2

---
+++ src/Chapter_2_DataTypes_and_Operators/VariantC/Task_2.java
@@ -0,0 +1,47 @@
+package Chapter_2_DataTypes_and_Operators.VariantC;
+
+import java.util.Random;
+
+/**
+ * Created by Ingener_mobil on 24.10.2016.
+ */
+public class Task_2 {
+    public static void main(String[] args) {
+        int rows = 8;
+        int cols = 8;
+        int moveLeft = 2;
+        Random rand = new Random();
+        int [][]matrix = new int[rows][cols];
+        int [] mas = new int[rows];
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                matrix[r][c] = rand.nextInt(100);
+                System.out.printf("%4d",matrix[r][c]);
+                if (c == rows - 1)
+                    System.out.println();
+            }
+        }
+        Move(matrix,moveLeft);
+
+    }
+
+    private static void Move(int[][] matrix,int move) {
+        System.out.println();
+        System.out.println();
+        int tempMatrix;
+        for (int r = 0; r < matrix.length; r++) {
+            for (int c = 0+move; c < matrix[r].length + move; c++) {
+                if (c + move == matrix.length + 2)
+                    c = 0;
+                if (c + move == matrix.length + 3)
+                    c = 1;
+                System.out.printf("%4d",matrix[r][c]);
+                if (c == move - 1) {
+                    System.out.println();
+                    c = matrix.length+move;
+                }
+            }
+        }
+    }
+}
+
