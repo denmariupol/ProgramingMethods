08baa2f51beecdbc7d7eeb576520207e60d9b3b9
den <denmariupol@mail.ru>
2016-10-26 13:41:26 +0300

    chapter2_C_tTask8

--- src/Chapter_2_DataTypes_and_Operators/VariantC/Task_8.java
+++ src/Chapter_2_DataTypes_and_Operators/VariantC/Task_8.java
@@ -1,5 +1,6 @@
 package Chapter_2_DataTypes_and_Operators.VariantC;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
@@ -14,7 +15,7 @@ public class Task_8 {
         int[] mas = new int[rows];
         for (int r = 0; r < rows; r++) {
             for (int c = 0; c < cols; c++) {
-                matrix[r][c] = rand.nextInt(100)-50;
+                matrix[r][c] = rand.nextInt(100) - 50;
                 System.out.printf("%4d", matrix[r][c]);
                 if (c == rows - 1)
                     System.out.println();
@@ -25,20 +26,40 @@ public class Task_8 {
 
     private static void Determinator(int[][] matrix) {
         System.out.println();
-        int det;
-        int [] d = new int[matrix.length];
-        for (int i = 1; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if
-                System.out.printf("%4d", matrix[i][j]);
-                if (j == matrix.length - 1)
-                    System.out.println();
-//                d[i] = matrix[i][j]*(Result(matrix[i][j]));
+        int[] d = new int[matrix.length];
+        int determinator = 0;
+        for (int k = 0; k < matrix.length; k++) {
+            ArrayList<ArrayList <Integer>> dMatrixOut = new ArrayList<ArrayList<Integer>>();
+
+
+            for (int i = 0; i < matrix.length; i++) {
+                if (k == i)
+                    continue;
+                ArrayList<Integer> dMatrixIn = new ArrayList<Integer>();
+                for (int j = 0; j < matrix[i].length; j++) {
+                    if (k == j)
+                        continue;
+                    dMatrixIn.add(matrix[i][j]);
+                }
+                dMatrixOut.add(dMatrixIn);
             }
+
+            d[k] = Result(dMatrixOut);
         }
+        for (int i = 0; i < d.length; i++) {
+            determinator+=d[i];
+        }
+        System.out.println(determinator);
     }
 
-//    private static int Result(int m){
-//
-//    }
+    private static int Result(ArrayList<ArrayList <Integer>> m) {
+         return m.get(0).get(0) * m.get(1).get(1) * m.get(2).get(2)+
+                m.get(0).get(1) * m.get(1).get(2) * m.get(2).get(0) +
+                m.get(0).get(2) * m.get(1).get(0) * m.get(2).get(1) -
+
+                m.get(0).get(2) * m.get(1).get(1) * m.get(0).get(2) -
+                m.get(0).get(0) * m.get(1).get(2) * m.get(2).get(1) -
+                m.get(0).get(1) * m.get(1).get(0) * m.get(2).get(2);
+
+    }
 }
