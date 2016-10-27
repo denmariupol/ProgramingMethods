df58772bd278ae12b3a8759605f36c49408a637a
den <denmariupol@mail.ru>
2016-10-27 08:23:21 +0300

    chapter2_C_tTask10

--- src/Chapter_2_DataTypes_and_Operators/VariantC/Task_10.java
+++ src/Chapter_2_DataTypes_and_Operators/VariantC/Task_10.java
@@ -3,41 +3,47 @@ package Chapter_2_DataTypes_and_Operators.VariantC;
 import java.util.Random;
 
 /**
- * Created by Ingener_mobil on 26.10.2016.
+ * Created by Ingener_mobil on 27.10.2016.
  */
-public class Task_9 {
+public class Task_10 {
     public static void main(String[] args) {
         int rows = 4;
         int cols = 4;
         Random rand = new Random();
-        float[][] matrix = new float[rows][cols];
+        int[][] matrix = new int[rows][cols];
         int[] mas = new int[rows];
         for (int r = 0; r < rows; r++) {
             for (int c = 0; c < cols; c++) {
                 matrix[r][c] = rand.nextInt(100) - 50;
-                System.out.printf("%7.0f", matrix[r][c]);
+                System.out.printf("%4d", matrix[r][c]);
                 if (c == rows - 1)
                     System.out.println();
             }
         }
-        Result(matrix);
+        MaxElement(matrix);
     }
 
-    private static void Result(float[][] matrix) {
-        System.out.println();
-        float sum = 0;
-        float mid;
+    private static void MaxElement(int[][] matrix) {
+        int max = matrix[0][0];
+        int maxI = 0,maxJ = 0;
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                sum+=matrix[i][j];
+            for (int j = 1; j < matrix[i].length; j++) {
+                if (matrix[i][j] > max) {
+                    max = matrix[i][j];
+                    maxI = i;
+                    maxJ = j;
+                }
             }
         }
-        mid = sum/(matrix.length * matrix[0].length);
+        System.out.println("Максимальный элемент "+max);
 
         for (int i = 0; i < matrix.length; i++) {
+            if (i == maxI)
+                continue;
             for (int j = 0; j < matrix[i].length; j++) {
-                matrix[i][j] = matrix[i][j] - mid;
-                System.out.printf("%7.2f", matrix[i][j]);
+                if(j == maxJ)
+                    continue;
+                System.out.printf("%4d", matrix[i][j]);
                 if (j == matrix.length - 1)
                     System.out.println();
             }
