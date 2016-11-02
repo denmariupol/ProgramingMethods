ed0106dab6c0808747bfb09d6afd38eec2029fd2
denmariupol <denmariupol@mail.ru>
2016-11-02 23:34:04 +0200

    Chapter_3_Variant_C_Task_19

---
+++ src/Chapter_3_Class_and_Objects/Variant_C/Array.java
@@ -0,0 +1,33 @@
+package Chapter_3_Class_and_Objects.Variant_C;
+
+/**
+ * Created by den on 2016-11-02.
+ */
+public class Array {
+
+    public static void bubble(int [] array){
+
+    }
+
+    public static void changeSort(int [] array) {
+
+    }
+    public static void shakerSort(int [] array) {
+
+    }
+    public static void simpleChoose(int [] array) {
+
+    }
+    public static void insertSort(int [] array) {
+
+    }
+    public static void hashSort(int [] array) {
+
+    }
+    public static void binSort(int [] array) {
+
+    }
+    public static void shellSort(int [] array) {
+
+    }
+}
---
+++ src/Chapter_3_Class_and_Objects/Variant_C/Programm.java
@@ -0,0 +1,11 @@
+package Chapter_3_Class_and_Objects.Variant_C;
+
+/**
+ * Created by den on 2016-11-02.
+ * 
+ */
+public class Programm {
+    public static void main(String[] args) {
+
+    }
+}
