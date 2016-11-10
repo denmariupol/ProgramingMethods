b5036d4608552b56698f5e488106187a11c0e37c
den <denmariupol@mail.ru>
2016-11-10 22:22:43 +0300

    Chapter_7_Variant_A_Task_20

--- src/Chapter_7_Strings/Variant_A/Task_20/Programm.java
+++ src/Chapter_7_Strings/Variant_A/Task_20/Programm.java
@@ -1,43 +1,36 @@
-package Chapter_7_Strings.Variant_A.Task_19;
+package Chapter_7_Strings.Variant_A.Task_20;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
  * Created by Ingener_mobil on 10.11.2016.
- * Преобразовать текст так, чтобы каждое слово,
- * не содержащее неалфавитных символов, начиналось с заглавной буквы.
  */
 public class Programm {
     public static void main(String[] args) {
         File file = new File("E:\\Lessons\\ProgramingMethods\\src\\Chapter_7_Strings\\Variant_A\\Task_1\\Source.txt");
         Scanner scanner = null;
+        int count = 0;
         try {
             scanner = new Scanner(file);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        StringBuilder sb = new StringBuilder();
         while (scanner.hasNextLine()) {
             String str = scanner.nextLine();
             Scanner scanner1 = new Scanner(str);
             while (scanner1.hasNext()) {
                 String word = scanner1.next();
-                Pattern pat = Pattern.compile("\\b\\D+\\p{Punct}*");
+                Pattern pat = Pattern.compile("\\p{Punct}");
                 Matcher matcher = pat.matcher(word);
                 while (matcher.find()) {
-                    char []w = matcher.group().toCharArray();
-                    w[0]=Character.toUpperCase(w[0]);
-//                    System.out.println(w[0]);
-                    sb.append(new String(w) + " ");
+                    count++;
                 }
             }
-            sb.append("\n");
         }
-        System.out.println(sb.toString());
+        System.out.println("Колличество знаков: "+count);
     }
 }
